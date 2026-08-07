1d8a399195f3950dd2d6e53e442e0f233aaa025e
nicolas-droppa <droppa.nicolas@gmail.com>
2026-08-07 23:24:17 +0200

    Add new and create note & links

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,4 +6,13 @@ class NotesController < ApplicationController
     def show
         @note = Note.find(params[:id])
     end
+
+    def new
+    end
+
+    def create
+        @note = Note.create(name: params[:name], body: params[:body])
+
+        redirect_to note_path(@note)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
-  get "notes", to: "notes#index"
-  get "notes/:id", to: "notes#show"
+  get 'notes', to: 'notes#index'
+  post 'notes', to: 'notes#create'
+  get 'notes/new', to: 'notes#new'
+  get 'notes/:id', to: 'notes#show', as: :note
 
-  get "items", to: "items#index"
-  get "items/:id", to: "items#show"
+  get 'items', to: 'items#index'
+  get 'items/:id', to: 'items#show'
 
   root 'pages#home'
 end
