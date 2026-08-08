c722470ed66dfdc6c484146918cbaa3aa47affc6
nicolas-droppa <droppa.nicolas@gmail.com>
2026-08-08 23:34:17 +0200

    Add set_note before_action to note controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,10 +1,11 @@
 class NotesController < ApplicationController
+    before_action :set_note, only: [:show, :edit, :update, :destroy]
+    
     def index
         @notes = Note.all
     end
     
     def show
-        @note = Note.find(params[:id])
     end
 
     def new
@@ -18,20 +19,15 @@ class NotesController < ApplicationController
     end
 
     def edit
-        @note = Note.find(params[:id])
     end
 
     def update
-        @note = Note.find(params[:id])
-
         @note.update(note_params)
 
         redirect_to note_path(@note)
     end
 
     def destroy
-        @note = Note.find(params[:id])
-
         @note.delete
 
         redirect_to notes_url
@@ -41,5 +37,9 @@ class NotesController < ApplicationController
 
     def note_params
         params.require(:note).permit(:name, :body)
+    end
+
+    def set_note
+        @note = Note.find(params[:id])
     end
 end
